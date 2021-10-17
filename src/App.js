53fe19ef5840de266b2e2d653ac19fb2dee35aec
Vukovic Emilie <emilievukovic79@gmail.com>
2021-10-17 03:09:53 +0200

    move fetch app => Home + add Loader

--- src/App.js
+++ src/App.js
@@ -9,41 +9,16 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            isLoaded: false,
-            accommodations: [],
-        };
-    }
-
-    componentDidMount() {
-        fetch(
-            'https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P9+React+1/logements.json'
-        )
-            .then(response => response.json())
-            .then(data => {
-                this.setState({ isLoaded: true, accommodations: data });
-                // console.log(this.state);
-                // console.log(data);
-            })
-            .catch(error => console.log(error, 'I have an error'));
-    }
-
     render() {
-        const { accommodations } = this.state;
-        // console.log(accommodations);
-
         return (
             <Router>
                 <div className="Wrap-Kasa">
                     <Header />
                     <Switch>
-                        {/* <Route exact path="/" component={Home} /> */}
-                        <Route exact path="/">
+                        <Route exact path="/" component={Home} />
+                        {/* <Route exact path="/">
                             <Home accommodations={accommodations} />
-                        </Route>
+                        </Route> */}
                         <Route path="/a-propos" component={About} />
                         <Route
                             path="/accommodation/:id"
